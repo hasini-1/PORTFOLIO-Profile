1930191878f9485e7f6bd04fdb83545f31c2643c
hasini-1 <balasrihasinikoppuravuri@gmail.com>
2026-07-19 10:39:08 +0530

    Folder Name changes

---
+++ Js/script.js
@@ -0,0 +1,43 @@
+const resumeButton = document.querySelector(".resume-btn");
+
+resumeButton.addEventListener("click", () => {
+
+    alert("Your resume download will begin shortly.");
+
+});
+
+function sendMail(event) {
+    event.preventDefault();
+
+    const params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        subject: document.getElementById("subject").value,
+        message: document.getElementById("message").value,
+    };
+
+    emailjs.send("service_qhj77x5", "template_vtr0554", params)
+        .then(function(response) {
+            console.log("SUCCESS!", response);
+            alert("Message sent successfully!");
+            document.querySelector("form").reset();
+        })
+        .catch(function(error) {
+            console.error("FAILED...", error);
+            alert("Failed to send message.");
+        });
+}
+
+const menu = document.getElementById("menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+menu.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+const links = document.querySelectorAll(".nav-links a");
+
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove("active");
+    });
+});
